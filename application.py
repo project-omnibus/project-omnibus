80479fbec73fafc63c1d91c9123806549bde8045
holistudio <daniel.bin.lu@gmail.com>
2018-09-28 23:36:55 -0400

    intial HTML and Flask server files
    intial HTML and Flask server files

---
+++ application.py
@@ -0,0 +1,22 @@
+import os
+import requests
+
+from flask import Flask, session, render_template
+from flask_socketio import SocketIO, emit
+from flask_session import Session
+
+app = Flask(__name__)
+
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+socketio = SocketIO(app, manage_session=False)
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+@app.route("/")
+def index():
+	return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
