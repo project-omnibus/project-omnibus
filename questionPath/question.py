e911ebf028e1f3f910b98c0d7c2649021c074873
robert2030ca <robert2030ca@gmail.com>
2018-10-03 11:51:07 -0400

    added question.csv, updated files in questionPath folder,

--- questionPath/question.py
+++ questionPath/question.py
@@ -7,5 +7,9 @@ possible_responses = [] #array of strings
 followUpBy = [] #array of integers for the questions that can follow this 
 relevancy = 100 #the current relevancy assignment for this question
 intent = [] #array of strings that specifies what this question asks for
+similarTo = [] #array of integer for the questions that is similar to this
+specificity=[] #an integer that indicate how specific or pointed the question is. Should general go from less specific to more specific questions
 
 
+def makeQuestion(sqlinput)
+	
