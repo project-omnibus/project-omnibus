d17fd7d90a7fe65cc524463c9d9868cdde07bc3e
robert2030ca <robert2030ca@gmail.com>
2018-10-11 21:21:57 -0400

    edited more code

--- questionPath/main.py
+++ questionPath/main.py
@@ -1,4 +1,27 @@
 import pandas as pd
+from question import *
+
 questionlist = pd.read_csv('questions.csv')
-questionrow = questionlist.iloc[[1]]
-print(questionrow['Question'])
+questionrow = pd.Series(questionlist.iloc[1])
+questiontext = questionrow.get('Question')
+qdict=questionrow.to_dict()
+s={'followUpBy':1}
+
+print(qdict)
+
+def makeQuestion(qObject,questionarray): #the input to this function needs to be a dictionary variable of 1 row of the data
+		qObject.followUpBy = questionarray['followUpBy']
+		qObject.possible_responses = questionarray['Possible Answer']
+		if len(qObject.possible_responses) == 0:
+			qObject.response_type = 0
+		else:
+			qObject.response_type = 1
+		qObject.similarTo = questionarray['similarTo']
+		qObject.text = questionarray['Question']
+
+
+q1 = question()
+makeQuestion(q1,qdict)
+#q1.runtest(1)
+
+#print(q1.text)
--- questionPath/question.py
+++ questionPath/question.py
@@ -1,25 +1,29 @@
-import panda
-class question
 
-index = 0
-text = "this is the text of the question"
-response_type = 0 # type of response expected, 0 for written, 1 for multiple choice
-possible_responses = [] #array of strings
-followUpBy = [] #array of integers for the questions that can follow this 
-relevancy = 100 #the current relevancy assignment for this question
-intent = [] #array of strings that specifies what this question asks for
-similarTo = [] #array of integer for the questions that is similar to this
-specificity=[] #an integer that indicate how specific or pointed the question is. Should general go from less specific to more specific questions
+class question:
+
+	index = 0
+	text = "this is the text of the question"
+	response_type = 0 # type of response expected, 0 for written, 1 for multiple choice
+	possible_responses = [] #array of strings
+	followUpBy = [] #array of integers for the questions that can follow this 
+	relevancy = 100 #the current relevancy assignment for this question
+	intent = [] #array of strings that specifies what this question asks for
+	similarTo = [] #array of integer for the questions that is similar to this
+	specificity=[] #an integer that indicate how specific or pointed the question is. Should general go from less specific to more specific questions
 
 
-def makeQuestion(questionarray) #the input to this function needs to be a panda data frame of 1 row
-	this.followUpBy = questionarray['followUpBy']
-	this.possible_responses = questionarray['Possible Answer']
-	if len(possible_responses) == 0:
-		this.response_type = 0
-	else:
-		this.response_type = 1
-	this.similarTo = questionarray['similarTo']
+	# def makeQuestion(questionarray): #the input to this function needs to be a dictionary variable of 1 row of the data
+	# 	followUpBy = questionarray['followUpBy']
+	# 	possible_responses = questionarray['Possible Answer']
+	# 	if len(possible_responses) == 0:
+	# 		response_type = 0
+	# 	else:
+	# 		response_type = 1
+	# 	similarTo = questionarray['similarTo']
+	# 	text = questionarray['Question']
+
+	# def runtest(array):
+	# 	print(array['followUpBy'])
 
 
 
