2fa123b0f37ee75d9bf33d2b612964b24b015bcb
robert2030ca <robert2030ca@gmail.com>
2018-10-14 19:59:31 -0400

    changed database and updated main.py

--- questionPath/main.py
+++ questionPath/main.py
@@ -1,27 +1,58 @@
 import pandas as pd
 from question import *
+import csv
+import os
 
-questionlist = pd.read_csv('questions.csv')
-questionrow = pd.Series(questionlist.iloc[1])
-questiontext = questionrow.get('Question')
-qdict=questionrow.to_dict()
-s={'followUpBy':1}
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 
-print(qdict)
+engine = create_engine(os.getenv("OMNIBUS_DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
 
-def makeQuestion(qObject,questionarray): #the input to this function needs to be a dictionary variable of 1 row of the data
-		qObject.followUpBy = questionarray['followUpBy']
-		qObject.possible_responses = questionarray['Possible Answer']
-		if len(qObject.possible_responses) == 0:
-			qObject.response_type = 0
-		else:
-			qObject.response_type = 1
-		qObject.similarTo = questionarray['similarTo']
-		qObject.text = questionarray['Question']
+#questionlist = pd.read_csv('questions.csv')
+#questionrow = pd.Series(questionlist.iloc[1])
+#questiontext = questionrow.get('Question')
+#qdict=questionrow.to_dict()
+#s={'followUpBy':1}
+
+#wprint(qdict)
+
+def makeQuestion(qObject,qDict,qfollowup,qsimilarto,qanswer): #the input to this function needs to be a dictionary variable of 1 row of the data
+	qObject.text = qDict.question
+	qObject.needUserInput = qDict['need_user_input']
+	qObject.relevancy = qDict['default_relevancy']
+	qObject.index = qDict['question_id']
+	qObject.specificity = qDict['specificity']
+	qObject.followUpBy = qfollowup
+	qObject.similarTo = qsimilarto
+	qObject.possibleAnswer = qanswer
 
 
-q1 = question()
-makeQuestion(q1,qdict)
+#make the question objects for the session
+numQuestions = db.execute("SELECT * FROM question_table").rowcount
+questionlist =[]
+print(numQuestions)
+followuparray = []
+similartoarray = []
+possibleanswerarray = []
+
+for numRow in range(numQuestions):
+	questionRow = db.execute("SELECT * FROM question_table WHERE question_id = :row",{"row":numRow+1}).fetchone()
+	for followRow in db.execute("SELECT * FROM question_followup_table WHERE question_id = :row",{"row":numRow+1}).fetchall():
+		followuparray.append(followRow.follow_up_by_id)
+	for similartoRow in db.execute("SELECT * FROM question_similarto_table WHERE question_id = :row",{"row":numRow+1}).fetchall():
+		similartoarray.append(similartoRow.similer_to_id)
+	for possibleanswerRow in db.execute("SELECT * FROM question_possibleanswer_table WHERE question_id = :row",{"row":numRow+1}).fetchall():
+		possibleanswerarray.append(possibleanswerRow.possible_answer)
+	q1 = question()
+	#print(questionRow)
+	makeQuestion(q1,questionRow,followuparray,similartoarray,possibleanswerarray)
+	questionlist.append(q1)
+
+print(questionlist[1].text)
+
+#q1 = question()
+#makeQuestion(q1,qdict)
 #q1.runtest(1)
 
-print(q1.followUpBy)
+#print(q1.followUpBy)
--- questionPath/question.py
+++ questionPath/question.py
@@ -3,14 +3,14 @@ class question:
 
 	index = 0
 	text = "this is the text of the question"
-	response_type = 0 # type of response expected, 0 for written, 1 for multiple choice
-	possible_responses = [] #array of strings
+	responseType = 0 # type of response expected, 0 for written, 1 for multiple choice
+	possibleAnswer = [] #array of strings
 	followUpBy = [] #array of integers for the questions that can follow this 
 	relevancy = 100 #the current relevancy assignment for this question
 	intent = [] #array of strings that specifies what this question asks for
 	similarTo = [] #array of integer for the questions that is similar to this
 	specificity=[] #an integer that indicate how specific or pointed the question is. Should general go from less specific to more specific questions
-
+	needUserInput = 0 #does this question need an user input to fill in the blanks
 
 	# def makeQuestion(questionarray): #the input to this function needs to be a dictionary variable of 1 row of the data
 	# 	followUpBy = questionarray['followUpBy']
@@ -24,6 +24,8 @@ class question:
 
 	# def runtest(array):
 	# 	print(array['followUpBy'])
+
+	#def __init__(self, questiontext,)
 
 
 
