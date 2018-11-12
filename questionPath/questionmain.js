5ddd52abf582c317caf780a734523e32b15a48ca
robert2030ca <robert2030ca@gmail.com>
2018-11-12 18:51:46 -0500

    added js file of questionmain

---
+++ questionPath/questionmain.js
@@ -0,0 +1,62 @@
+/* This code is a copy of questionmain.py to implement the "findBestResponse"
+function in Javascript instead of Python because this function can be run 
+on the client side. 
+
+It assumes that getting the list of questions and the array of question object
+is already done. This function only pertains to the calculation on relevancy 
+attribute of each of the question object in the array, assigning the relevancy
+of current question to 0 and finding the question with highest relevancy
+*/
+
+
+function assignrelevancy (qObject, qObjectlist){
+	// Function assigns the relevancy of all questions in the question
+	//qObject is the question object of the current question that was just asked
+	//qObjectList is the complete array of all question objects, including the current one
+	qObjectList[(qObject.index-1)].relevancy = 0; //set the relevancy of the current question in question array to 0
+	var followUpLen = qObject.followUpBy.length; //get the length of the number of folloUpBy question to the current question
+	if (followUpLen != 0) { //if length isn't 0, then there is some questions that can follow up to current question, set relevancy of those questions to 100.
+		for (i = 0; i<followUpLen; i++) {
+			qObjectList[qObject.followUpBy[i]-1].relevancy=100*(qObject.followUpBy[i]-1].relevancy-9); //"-9" here beacuse the minimum default relevancy is 10 for now
+		}
+	}
+}
+
+function findmaxrelevancy (qObjectList){
+	//Function finds the maximum relevancy, indices of the questions with max relevancy and the entire relevancy list
+	//returns a maxrel object
+	//qObjectList is the complete array of all question objects
+	var relevancylist= new Array(); // insstantiate relevancylist variable
+	var maxrelevancyindex= new Array(); //instantiate maxrelevancyindex variable
+	var qlen = qObjectList.length; //get the length of the question object list
+	for (i = 0; i<qlen; i++){ //iterated through question object list and populates relevancy of each question to relevancylist var
+		relevancylist.push(qObjectList[i].relevancy);
+	}
+	var m=Math.max(relevancylist); //find the value of the max relevancy in the list
+	for (i = 0; i<qlen; i++) { //iterate through and populates maxrelevancyindex var with indices of questions with max relevancy
+		if (qObjectList[i].relevancy==m){
+			maxrelevancyindex.push(i);
+		}
+	}
+	var maxrelList = {index:maxrelevancyindex, value:m, array:relevancylist};
+	return maxrelList
+}
+
+function findquestion(qObjectList,maxrelList){
+	//function finds and returns 1 question object to present to user. 
+	//qObjectList is the complete array of all question objects
+	//maxrelList is an array of the indices of questions with max relevancy. 
+	if (maxrelList.length) == 1 {
+		return qObjectList[maxrelList[0]];
+	}
+	else if (maxrelL)
+}
+	if len(relList) == 1:
+		return qObjectList[relList[0]]
+	elif len(relList) > 1:
+		return qObjectList[relList[random.randint(0,len(relList)-1)]]
+	else:
+		x=random.randint(0,23)
+		while qObjectList[x].relevancy == 0:
+			x=random.randint(0,23)
+		return qObjectList[x]
