a99c21d1a253a5ed948e3e51b267afd9b4947ba3
robert2030ca <robert2030ca@gmail.com>
2018-11-13 22:43:42 -0500

    deleted application.py

--- questionPath/qdemoscript.js
+++ questionPath/qdemoscript.js
@@ -1,25 +1,26 @@
 var qfunctions = require('./qfunction.js');
-const {Pool, Client} = require('pg');
+var pg = require('pg');
+pg.defaults.ssl = true;
 var http = require('http');
+
 
 var databaseURL = process.env.OMNIBUS_DATABASE_URL;
 var databaseHost = process.env.OMNIBUS_DATABASE_HOST;
 var databaseUser = process.env.OMNIBUS_DATABASE_USERNAME;
 var databasePassword = process.env.OMNIBUS_DATABASE_PASSWORD;
 
-const pool = new Pool({
+var client = new pg.Client({
 	connectionString: databaseURL,
 });
 
-pool.query('SELECT NOW()', (err, res) =>{
-	console.log(err,res);
-	pool.end();
-})
+client.connect();
+
+
 
 http.createServer(function (req, res) {
-	fs.readFile('demo.html', function(err, data){
+
 		res.writeHead(200, {'Content-Type': 'text/html'});
-    	res.write (uc('Hello World'));
+    	res.write ('Hello World');
     	res.end();	
-	});
+
 }).listen(8080);
