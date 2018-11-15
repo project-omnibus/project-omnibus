1f3a6d8a850b20eff8428039871099bdccac5ac6
evilmishie <43704002+evilmishie@users.noreply.github.com>
2018-11-14 23:27:44 -0500

    getting query to work

--- questionPath/qdemoscript.js
+++ questionPath/qdemoscript.js
@@ -13,7 +13,20 @@ var client = new pg.Client({
 	connectionString: databaseURL,
 });
 
+var questionList = [];
+function setquestionList(result){
+	questionList = result;
+	console.log(questionList.rows[0].question);
+}
+
 client.connect();
+client.query("SELECT * FROM question_table", function (err, result, fields){
+	if (err) {
+		throw err;
+	} else {
+		setquestionList(result);
+	}
+});
 
 
 
@@ -21,6 +34,7 @@ http.createServer(function (req, res) {
 
 		res.writeHead(200, {'Content-Type': 'text/html'});
     	res.write ('Hello World');
+    	res.write (questionList.rows[0].question);
     	res.end();	
 
 }).listen(8080);
