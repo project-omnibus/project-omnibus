ce2b74de365ebae47779605fe737b74dabe1754e
robert2030ca <robert2030ca@gmail.com>
2018-11-28 12:34:23 -0500

    added conversation html, get and post request.js. Trying to use jquery ajax for conversation page

---
+++ client/src/getrequest.js
@@ -0,0 +1,28 @@
+$( document ).ready(function() {
+	
+	// GET REQUEST
+	$("#allAttribute").click(function(event){
+		event.preventDefault();
+		ajaxGet();
+	});
+	
+	// DO GET
+	function ajaxGet(){
+		$.ajax({
+			type : "GET",
+			url : window.location + "/conversation",
+			success: function(result){
+				$('#getResultDiv ul').empty();
+				var attributeList = "";
+				$.each(result, function(i, user){
+					$('#getResultDiv .list-group').append(user.attribute+ "<br>")
+				});
+				console.log("Success: ", result);
+			},
+			error : function(e) {
+				$("#getResultDiv").html("<strong>Error</strong>");
+				console.log("ERROR: ", e);
+			}
+		});	
+	}
+})
