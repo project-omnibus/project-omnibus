c5bf274a63ff03ce342cde1389713b3f498790d2
holistudio <daniel.bin.lu@gmail.com>
2018-12-20 17:18:35 -0500

    removed styling css from unnecessary places

--- shared/components/HTML.js
+++ shared/components/HTML.js
@@ -6,7 +6,6 @@ const HTML = (props) => (
       <meta charSet="utf-8"/>
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossOrigin="anonymous"/>
-      <link rel="stylesheet" href="../../client/src/index.css"/>
       <meta name="theme-color" content="#000000"/>
       <title>Project Omnibus</title>
     </head>
