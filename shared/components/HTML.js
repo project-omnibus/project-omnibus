99bd89f9b3f7d1758d390bfe2cd09880cf2cd475
holistudio <daniel.bin.lu@gmail.com>
2019-01-03 22:19:25 -0500

    HTML.js update, favicon added
    static files will go directly into build/static/. will look into how webpack can load these automatically later

--- shared/components/HTML.js
+++ shared/components/HTML.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
 const HTML = (props) => (
   <html className="index" lang="en">
     <head>
       <meta charSet="utf-8"/>
+      <link rel="shortcut icon" href="/static/favicon.ico"/>
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossOrigin="anonymous"/>
       <meta name="theme-color" content="#000000"/>
