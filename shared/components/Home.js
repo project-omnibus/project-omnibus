f04fd93727f7da947d0abecfdc6e55b90c423ab3
holistudio <daniel.bin.lu@gmail.com>
2019-01-04 00:05:36 -0500

    hompage with navbar and chatbot message components

--- shared/components/Home.js
+++ shared/components/Home.js
@@ -2,24 +2,35 @@ import React from 'react';
 import { Switch } from 'react-router-dom';
 import { renderRoutes } from 'react-router-config';
 import Nav from './Navigation'
+import Conversation from './Conversation'
+import ChatbotMessageDialogBubble from './ChatbotMessageDialogBubble'
+
 class Home extends React.Component{
 
 	constructor(props){
 		super(props);
-		this.state = {}
+		this.state = {};
 	}
 
 	render(){
     console.log('rendering:home')
 		return (
-      <div>
-        <Nav route ={this.props.route}/>
-				<div id='greeting'>
-					<h1>Hello this is Home Page</h1>
-					<p>{this.props.mainProps}</p>
+			<div>
+				<Nav route ={this.props.route}/>
+				<div className="container">
+					<ChatbotMessageDialogBubble />
 				</div>
 			</div>
 		);
+		// return (
+    //   <div>
+    //     <Nav route ={this.props.route}/>
+		// 		<div id='greeting'>
+		// 			<h1>Hello this is Home Page</h1>
+		// 			<p>{this.props.mainProps}</p>
+		// 		</div>
+		// 	</div>
+		// );
 	}
 }
 export default Home;
