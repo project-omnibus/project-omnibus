0ed6ebb3ffb34c0eeaf91e8b9cc7bde1b1a99bbe
holistudio <daniel.bin.lu@gmail.com>
2019-01-04 00:02:41 -0500

    Brand Component logo png added

--- shared/components/Brand.js
+++ shared/components/Brand.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
 class Brand extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {};
+  }
 
+  render(){
+    return (
+      <a className="navbar-brand" href="#"><img height="110" src="/static/Navigation/icons/icon-placeholder.png"/></a>
+
+    );
+  }
 };
 
 export default Brand;
