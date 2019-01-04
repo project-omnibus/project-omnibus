fb74a6f6d025d4de52fe478b5a167de0d4928f04
holistudio <daniel.bin.lu@gmail.com>
2019-01-04 00:04:08 -0500

    ChatbotMessageDialogBubble Component updated

--- shared/components/ChatbotMessageDialogBubble.js
+++ shared/components/ChatbotMessageDialogBubble.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import ChatbotMessage from './ChatbotMessage';
 
 class ChatbotMessageDialogBubble extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {};
+  }
 
+  render(){
+    return(
+      <div>
+        <ChatbotMessage />
+      </div>
+    );
+
+  }
 };
 
 export default ChatbotMessageDialogBubble;
