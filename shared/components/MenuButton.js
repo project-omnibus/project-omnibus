e1e710326466f30e4cd93128e9117fa544c1da50
holistudio <daniel.bin.lu@gmail.com>
2019-01-04 00:03:01 -0500

    MenuButton updated with png

--- shared/components/MenuButton.js
+++ shared/components/MenuButton.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
 class MenuButton extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {};
+  }
 
+  render(){
+    return (
+      <img height="110" src="/static/Navigation/icons/menu-icon.png"/>
+    );
+  }
 };
 
 export default MenuButton;
