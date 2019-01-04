18900fa3fdda0f2921be6f33d5db95eaef4d89e5
holistudio <daniel.bin.lu@gmail.com>
2019-01-04 00:03:54 -0500

    ChatbotMessage Component updated

--- shared/components/ChatbotMessage.js
+++ shared/components/ChatbotMessage.js
@@ -1,7 +1,25 @@
 import React from 'react';
+// import styles from '../styles/ChatbotMessage.css'
 
 class ChatbotMessage extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      chatbot_message: {
+        message: "",
+        datetime: "0000-00-00 00:00:00",
+      },
+    };
+  }
 
+  render(){
+    return(
+      <div>
+        What's the last good book you read?
+      </div>
+    );
+
+  }
 };
 
 export default ChatbotMessage;
