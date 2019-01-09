bef82249dbf3cc89919cadfd3e4c0f3aafa3a43c
holistudio <daniel.bin.lu@gmail.com>
2019-01-09 01:03:53 -0500

    HTML.js references the correct style sheet

--- shared/components/HTML.js
+++ shared/components/HTML.js
@@ -7,6 +7,7 @@ const HTML = (props) => (
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossOrigin="anonymous"/>
       <meta name="theme-color" content="#000000"/>
+      <link rel="stylesheet" href="./styles.css"/>
       <title>Project Omnibus</title>
     </head>
     <body>
