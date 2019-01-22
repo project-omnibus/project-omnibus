c2319bfc89bbc8cabc91e84e9cdae182dbd5454b
holistudio <daniel.bin.lu@gmail.com>
2019-01-22 18:27:22 -0500

    visual styling of chatbot conversation box

--- shared/components/ChatbotMessageDialogBubble.js
+++ shared/components/ChatbotMessageDialogBubble.js
@@ -1,9 +1,10 @@
 import React from 'react'; // imports allows for JSX syntax
 import ChatbotMessage from './ChatbotMessage';
+import '../styles/ChatbotMessageDialogBubble.css';
 
 const ChatbotMessageDialogBubble = props => {
   return (
-    <div>
+    <div className='chatbotBubbleWrapper'>
       <ChatbotMessage message={props.message} />
     </div>
   );
