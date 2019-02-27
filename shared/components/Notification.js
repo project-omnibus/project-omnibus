955a0f54c2aeef950fbee9984dae61ac0ad61fe9
Daniel <daniel.bin.lu@gmail.com>
2019-02-27 15:54:09 -0500

    added buttons to notification message

--- shared/components/Notification.js
+++ shared/components/Notification.js
@@ -6,14 +6,24 @@ class Notification extends React.Component {
     super(props);
     this.state = {
       message: "Hey! Looks like this is your first time here. Let's talk about your reading interests and I'll make some recommendations.",
+      buttons: [
+        {type: 'submit',
+        text:'OK'},
+        {type: 'cancel',
+        text:'No thanks'},
+      ],
     };
   };
   render () {
+    let buttonDisplay = this.state.buttons.map((e,i) => {
+      return (<button className='btn'>{e.text}</button>);
+    });
     return(
       <div className='notifOverlay'>
-      <div className='bubble'>
-        <div className='messageText'>{this.state.message}</div>
-      </div>
+        <div className='bubble'>
+          <div className='messageText'>{this.state.message}</div>
+          {buttonDisplay}
+        </div>
       </div>
     );
   }
